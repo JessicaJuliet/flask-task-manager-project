7506e544aa6bb8d64bfbf1fe706a4e10e38b56f6
JessicaJuliet <jessica.akomfrah@gmail.com>
2021-05-03 11:19:55 +0000

    Fix bug which causes runtime error after submitting registratoin form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
-    Flask, render_template, redirect,
-    request, session, url_for)
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 app.config["MONGO_NAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.Secret_Key = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
